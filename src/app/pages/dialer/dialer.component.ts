30991d32564abed166071ed0624e05732f42d17f
Shubham Jaiswal <shubham.jaiswal357@gmail.com>
2021-07-24 01:18:04 +0530

    clipboard

--- src/app/pages/dialer/dialer.component.ts
+++ src/app/pages/dialer/dialer.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PhoneService } from 'src/app/services/phone.service';
+import { Clipboard } from '@capacitor/clipboard';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-dialer',
@@ -11,9 +13,22 @@ export class DialerComponent implements OnInit {
   digits = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['*', '0', '#']];
   phoneNumber: string = "";
 
-  constructor(private phoneService: PhoneService) { }
+  constructor(private phoneService: PhoneService, private common: CommonService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const checkClipboard = async () => {
+      const { type, value } = await Clipboard.read();
+
+      console.log(`Got ${type} from clipboard: ${value}`);
+      if (type == "text/plain") {
+        let rawText = value.replace(/\s/g, '').substr(-13, 13)
+        if (rawText.startsWith('+91') || rawText.startsWith('0') || rawText.startsWith('9') || rawText.startsWith('8') || rawText.startsWith('7') || rawText.startsWith('6'))
+          rawText.length >= 10 ? this.phoneNumber = rawText.substr(-10, 10) : null;
+        this.common.presentToast(`Got ${rawText} from clipboard`)
+      }
+    };
+    checkClipboard();
+  }
 
   dialIn(digit) {
     this.phoneNumber += digit;
