6e3faff6225acf81bb0f8d419acb87328772f879
Shubham Jaiswal <shubham.jaiswal357@gmail.com>
2021-09-25 00:14:41 +0530

    dev keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   clickupBaseUrl: "https://api.clickup.com/api/v2",
-  clickupApiKey: "pk_3425866_SK24KM1D8RDMZH81KG4AM7OFXYWV7YL8",
-  list_id: "11981038",
+  clickupApiKey: "pk_5444370_5X1SD7Z42QLIGY6IMPEVM3JWX4371TT9",
+  list_id: "49221350",
   clickiApi: "X4ouc5aQ6FQCNm6Z0Mno2IR-CV6gKU5VKXhLUQFVyYlUjo5UHjaMbdR0AZsWtGQo",
   clickiURL:"https://caller.clickiexpress.com/api_v2/voice-call",
   // customFieldId: "65db9101-b893-4969-8566-74e4928eafa7",
